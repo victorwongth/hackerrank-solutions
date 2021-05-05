a594b54cd73922cd8a98097cb212ea1af58efa44
victorwongth <victorwongth@gmail.com>
2021-05-05 22:01:32 +0800

    Add array manipulation solution

---
+++ interview-preparation-kit/arrays/array-manipulation/array-manipulation-v1.py
@@ -0,0 +1,52 @@
+# https://www.hackerrank.com/challenges/crush/problem
+# This solution work but if n, m and k are large the test cases timeout
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    l = [0] * n
+
+    # This adds the values per query then find the max from the list
+    for query in queries:
+        for i in range(query[0]-1, query[1]):
+            l[i] += query[2]
+
+    return(max(l))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
