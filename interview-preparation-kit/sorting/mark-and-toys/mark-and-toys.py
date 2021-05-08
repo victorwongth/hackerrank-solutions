99bc2cf23598aa02eecd3269d2a7ea0ab8f783ff
victorwongth <victorwongth@gmail.com>
2021-05-08 23:45:09 +0800

    Add mark-and-toys solution

---
+++ interview-preparation-kit/sorting/mark-and-toys/mark-and-toys.py
@@ -0,0 +1,48 @@
+# https://www.hackerrank.com/challenges/mark-and-toys/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    # Write your code here
+    cost = 0
+    total = 0
+    sorted_prices = sorted(prices)
+    for i in range(len(prices)):
+        total += 1
+        cost += sorted_prices[i]
+        if cost > k:
+            return(total - 1)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
