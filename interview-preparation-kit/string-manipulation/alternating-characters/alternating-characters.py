a202aa97f19067504f58ffeb7e96349c34761261
victorwongth <victorwongth@gmail.com>
2021-05-12 18:24:30 +0800

    Add alternating-characters solution

---
+++ interview-preparation-kit/string-manipulation/alternating-characters/alternating-characters.py
@@ -0,0 +1,38 @@
+# https://www.hackerrank.com/challenges/alternating-characters/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    deletion = 0
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            deletion += 1
+    return(deletion)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
